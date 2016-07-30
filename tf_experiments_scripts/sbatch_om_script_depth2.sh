6761545e3ea536c5c0e3409a191a567b43d5f1f8
Brando <brando90@mit.edu>
2016-07-30 13:46:44 -0400

    sbatch_

--- tf_experiments_scripts/sbatch_om_script_depth2.sh
+++ tf_experiments_scripts/sbatch_om_script_depth2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=HBF2_6_6
+#SBATCH --job-name=NN2_48_48
 #SBATCH --nodes=1
 #SBATCH --mem=7000
 #SBATCH --time=6-23
-#SBATCH --array=1-5
+#SBATCH --array=1-1000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2 task_29_july_NN2_10 NN1_12_12_multiple_10 True 12,12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2 task_28_july_NN2_1000_BN NN2_48_48_multiple_1000_BN True 48,48 multiple_S task_f_2d_task2_xsinglog1_x_depth2 True True
+
