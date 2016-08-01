22ec3a5037b0b30cde24af2fe0ae28af1b7b2279
Brando <brando90@mit.edu>
2016-08-01 11:04:41 -0400

    change

--- tf_experiments_scripts/sbatch_om_script_depth2_hbf1_12.sh
+++ tf_experiments_scripts/sbatch_om_script_depth2_hbf1_12.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_29_july_HBF1_depth_2_1000 HBF1_12_multiple_1000 True 12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_30_july_HBF1_depth_2_1000 HBF1_12_multiple_1000 True 12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
