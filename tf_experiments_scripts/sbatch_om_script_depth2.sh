0b38fadc522f47c8eba7e8dc43826564bb93a0e9
Brando <brando90@mit.edu>
2016-07-31 22:10:10 -0400

    magic sbatch

--- tf_experiments_scripts/sbatch_om_script_depth2.sh
+++ tf_experiments_scripts/sbatch_om_script_depth2.sh
@@ -8,3 +8,6 @@
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
 python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_29_july_HBF1_depth_2_10 HBF1_12_multiple_10 True 12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
+
+#python main_nn.py SLURM_JOBID SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_29_july_HBF1_depth_2_10 HBF1_12_multiple_10 True 12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
+#python ../main_nn.py SLURM_JOBID SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_29_july_HBF1_depth_2_10 HBF1_12_multiple_10 True 12 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
---
+++ tf_experiments_scripts/sbatch_om_script_depth2_hbf1_48.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=HBF1_48
+#SBATCH --nodes=1
+#SBATCH --mem=7000
+#SBATCH --time=6-23
+#SBATCH --array=1-10
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rene_sax14@yahoo.com
+
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_29_july_HBF1_depth_2_1000 HBF1_48_multiple_1000 True 48 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
