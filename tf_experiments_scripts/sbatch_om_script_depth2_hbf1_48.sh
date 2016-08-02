a499678d2ceda0d4eb3f21866168cc38abb70d49
Brando <brando90@mit.edu>
2016-08-02 02:41:43 -0400

    asdasd

--- tf_experiments_scripts/sbatch_om_script_depth2_hbf1_48.sh
+++ tf_experiments_scripts/sbatch_om_script_depth2_hbf1_48.sh
@@ -7,4 +7,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_xsinlog1_x_depth2_hbf task_30_july_HBF1_depth_2_1000 HBF1_48_multiple_1000 True 48 multiple_S task_f_2d_task2_xsinglog1_x_depth2 False False hbf kern_init
+#task_name=task_f2d_2x2_1_cosx1_plus_x2_depth_2
+task_name=task_f2d_2x2_1_cosx1x2_depth_2
+#task_name=task_f_2d_task2_xsinglog1_x_depth2
+folder=om_2x2_1_cosx1_plus_x2_depth_hbf
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID $folder task_30_july_HBF1_depth_2_1000 HBF1_48_multiple_1000 True 48 multiple_S $task_name False False hbf kern_init
