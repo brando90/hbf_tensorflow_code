e204935e66a168f686957365be75e536c81ee414
Brando <brando90@mit.edu>
2016-08-02 02:32:37 -0400

    more

---
+++ tf_experiments_scripts/sbatch_om_script_depth2_hbf2_48_48__x1_p_x2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=HBF2_48_48
+#SBATCH --nodes=1
+#SBATCH --mem=7000
+#SBATCH --time=6-23
+#SBATCH --array=1-1000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rene_sax14@yahoo.com
+
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID om_2x2_1_cosx1_plus_x2_depth_hbf task_1_August_HBF2_depth_2_1000 HBF2_48_48_multiple_1000 True 48,48 multiple_S task_f2d_2x2_1_cosx1_plus_x2_depth_2 False False hbf data_xavier_kern
