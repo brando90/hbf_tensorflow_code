b49a38c156f8ad3598266a74579a751abe9fc4f2
Brando <brando90@mit.edu>
2016-08-03 19:23:54 -0400

    sbatch

--- tf_experiments_scripts/sbatch_om_script_depth2_hbf1_96.sh
+++ tf_experiments_scripts/sbatch_om_script_depth2_hbf1_96.sh
@@ -6,6 +6,7 @@
 #SBATCH --array=1-1000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
+#SBATCH --exclude=node047
 
 task_name=task_f2d_2x2_1_cosx1_plus_x2_depth2
 #task_name=task_f2d_2x2_1_cosx1x2_depth2
@@ -18,4 +19,4 @@ data_normalize=dont_normalize
 #dont_train_S=train_S
 trainable_S=dont_train_S
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID $folder task_30_july_HBF1_depth_2_1000 HBF1_12_multiple_1000 True 96 multiple_S $task_name False False hbf kern_init $data_normalize $trainable_S
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID $folder task_1_August_HBF1_depth_2_1000_dont_train_S HBF1_96_multiple_1000 True 96 multiple_S $task_name False False hbf kern_init $data_normalize $trainable_S
