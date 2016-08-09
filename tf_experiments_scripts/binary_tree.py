829d4da1ba4765f429d1c3ab2470ec5c1c09d48a
Brando <brando90@mit.edu>
2016-08-09 13:47:10 -0400

    working on binary tree

--- tf_experiments_scripts/binary_tree.py
+++ tf_experiments_scripts/binary_tree.py
@@ -5,6 +5,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 import tensorflow as tf
 from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
+from tensorflow.examples.tutorials.mnist import input_data
 
 
 task_name = 'task_MNIST_flat_auto_encoder'
@@ -16,10 +17,11 @@ X_test, Y_test = mnist.test.images, mnist.test.labels
 # data shape is "[batch, in_height, in_width, in_channels]",
 # X_train = N x D
 N, D = X_train.shape
-x = tf.Variable( X_train ).reshape(N,1,D,1)
+X_train = X_train.reshape(N,1,D,1)
+x = tf.Variable( X_train )
 filter_size, K = 10, 12 # number of units
 W = tf.Variable( tf.truncated_normal(shape=[1,filter_size,1,K], stddev=0.1) )
-conv = tf.nn.conv2d(input=x, filter=W,strides=[1, 1, 1, 1], padding="SAME", name="conv")
+conv = tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding="SAME", name="conv")
 
 with tf.Session() as sess:
     sess.run(fetches=conv, feed_dict={x:X_train})
