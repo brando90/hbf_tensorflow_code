69c95e960cdd2674e358f0d5ffd939f4cae88a19
Brando <brando90@mit.edu>
2016-08-10 02:53:54 -0400

    solved merge

--- tf_experiments_scripts/batch_main.sh
+++ tf_experiments_scripts/batch_main.sh
@@ -1,1 +1,54 @@
-emtyp file`
+#!/bin/bash
+#SBATCH --job-name=HBF1_12
+#SBATCH --nodes=1
+#SBATCH --mem=14000
+#SBATCH --time=6-23
+#SBATCH --array=1-1000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rene_sax14@yahoo.com
+#SBATCH --gres=gpu:1
+
+#task_name=task_f2d_2x2_1_cosx1_plus_x2_depth2
+#task_name=task_f2d_2x2_1_cosx1x2_depth2
+#task_name=task_f_2d_task2_xsinglog1_x_depth2
+task_name=task_MNIST_flat_auto_encoder
+#main_folder=om_2x2_1_cosx1_plus_x2_depth2
+main_folder=om_mnist
+
+#data_normalize=normalize_input
+data_normalize=dont_normalize
+
+#trainable_S=train_S
+trainable_S=dont_train_S
+
+#mdl=standard_nn
+mdl=hbf
+
+#train_S_type=multiple_S
+#train_S_type=single_S
+#init_type=truncated_normal
+#init_type=data_init
+#init_type=kern_init
+#init_type=kpp_init
+#init_type=data_trunc_norm_kern
+init_type=data_trunc_norm_trunc_norm
+#init_type=data_xavier_kern
+#init_type=xavier
+
+#init=all_same_const-525.32626263
+#init=all_same_const-20.3010101
+#init=first_constant_rest_uniform_random-[525.32626263,[0.9,2.5]]
+init=first_constant_rest_specific_consts-[1250.32,3]
+init=first_rand_same_uniform_rest_uniform_random-[[1,1250.32],[2,4]]
+
+units=6,6
+
+#optimization_alg=GD
+#optimization_alg=Momentum
+#optimization_alg=Adadelta
+#optimization_alg=Adagrad
+optimization_alg=Adam
+#optimization_alg=RMSProp
+
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID $main_folder task_August_7_HBF2_dont_train_S_data_trunc_norm_kern HBF2_6_6_1000 True $units multiple_S $task_name False False $mdl $init_type $data_normalize $trainable_S $init $optimization_alg
+
