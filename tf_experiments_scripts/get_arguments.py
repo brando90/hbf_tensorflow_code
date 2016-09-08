e9569fa3d20d44aafa33b2db2087e9b60c2f14a9
Brando <brando90@mit.edu>
2016-09-08 00:35:40 -0400

    get argument

--- tf_experiments_scripts/get_arguments.py
+++ tf_experiments_scripts/get_arguments.py
@@ -1,14 +1,15 @@
-# Load the dictionary back from the pickle file. 
+# Load the dictionary back from the pickle file.
 import pickle
 import namespaces as ns
+import sys
 
 # arg_dict = dict(arg).copy()
 # arg_dict = get_remove_functions_from_dict(arg_dict)
 # pickle.dump( arg_dict, open( "slurm-%s_%s.p"%(arg.slurm_jobid,arg.slurm_array_task_id) , "wb" ) )
 
 if len(sys.argv) == 3:
-    arg.slurm_jobid = sys.argv[1]
-    arg.slurm_array_task_id = sys.argv[2]
+    slurm_jobid = sys.argv[1]
+    slurm_array_task_id = sys.argv[2]
 else:
     arg.slurm_jobid = 1
     arg.slurm_array_task_id = 2
