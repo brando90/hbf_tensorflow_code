f716f7bc371d29f23f72ef2eb038906cc5cecdd0
Brando <brando90@mit.edu>
2016-09-18 21:01:55 -0400

    first draft for NN BT

---
+++ my_tf_proj/my_tf_pkg/NN_general_bt.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import sklearn as sk
+from sklearn.metrics.pairwise import euclidean_distances
+
+import tensorflow as tf
+from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
+
+def bt_mdl(arg, x, W, l, left, right):
+    if l == 1:
+        z = x[:,left:right] * W[l] # (M x F^(1)) = (M x D) x (D x F^(1))
+        a = arg.act(z)  # (M x F^(1))
+        return a # (M x F^(l)) = (M x F^(1))
+    else:
+        dif = int((right - left)/2)
+        bt_left = bt_mdl(x, W, l-1, left, left+dif) # (M x F^(l-1))
+        bt_right = bt_mdl(x, W, l-1, left+dif, right) # (M x F^(l-1))
+        bt = bt_left + bt_right # (M x 2F^(l-1))
+        bt = bt * W[l] # (M x F^(l)) = (M x 2F^(l-1)) x (2F^(l-1) x F^(l))
+        return arg.act( bt )
