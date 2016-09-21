31e33f1aae91a140366fd1cbcd234662b0ee8c0b
Brando <brando90@mit.edu>
2016-09-20 20:55:25 -0400

    8D saving

--- my_tf_proj/my_tf_pkg/general_bt_NN.py
+++ my_tf_proj/my_tf_pkg/general_bt_NN.py
@@ -33,7 +33,12 @@ def bt_mdl_conv(arg,x):
     kernel_width = 2
     stride_width = kernel_width
     for l in range(arg.L-1,0,-1):
-        conv = get_activated_conv_layer(l,arg,x=conv,kernel_size=[1,kernel_width],stride=[1,kernel_width],scope='conv_'+str(l))
+        nb_layer = arg.L - l
+        print('l ', nb_layer)
+        print('kernel_width ', kernel_width)
+        print('stride_width ', stride_width)
+        print(conv)
+        conv = get_activated_conv_layer(nb_layer,arg,x=conv,kernel_size=[1,kernel_width],stride=[1,kernel_width],scope=arg.scope_name+str(nb_layer))
         # setup for next iteration
         kernel_width = 2*arg.F[l]
         stride_width = kernel_width
@@ -53,8 +58,9 @@ def get_activated_conv_layer(l,arg,x,kernel_size,stride,scope):
         normalizer_params=None,
         weights_initializer=arg.weights_initializer,
         biases_initializer=arg.biases_initializer,
+        scope=scope,
         trainable=True,
-        scope=scope
+        reuse=False
     )
     return conv
 
@@ -70,45 +76,47 @@ class TestNN_BT(unittest.TestCase):
         arg.weights_initializer = tf.constant_initializer(value=1.0, dtype=tf.float32)
         #biases_initializer = tf.constant_initializer(value=0.0, dtype=tf.float32)
         arg.biases_initializer = tf.constant_initializer(value=0.1, dtype=tf.float32)
-
         arg.normalizer_fn = None
         #arg.normalizer_fn = tf.contrib.layers.batch_norm
-
         arg.F = F
         return arg
 
     def test_NN_BT4D(self):
-        print('test')
-        D = 5
+        print('\n -------test')
+        D = 4
         x = tf.placeholder(tf.float32, shape=[None,1,D,1], name='x-input') #[M, 1, D, 1]
         # prepare args
         arg = self.get_args(L=2,F=[3,5])
+        arg.scope_name = 'BT4D'
         # get NN BT
         bt_mdl = bt_mdl_conv(arg,x)
         # do check
         M = 2
-        X_data = np.array( [np.arange(0,5),np.arange(5,10)] )
+        X_data = np.array( [np.arange(0,4),np.arange(4,8)] )
+        print('X_data ', X_data)
         X_data = X_data.reshape(M,1,D,1)
         with tf.Session() as sess:
             sess.run( tf.initialize_all_variables() )
-            print( sess.run(fetches=bt_mdl, feed_dict={x:X_data}) )
+            print('output: ', sess.run(fetches=bt_mdl, feed_dict={x:X_data}) )
         #self.assertTrue(correct)
 
     def test_NN_BT8D(self):
-        print('test')
-        D = 5
+        print('\n -------test')
+        D = 8
         x = tf.placeholder(tf.float32, shape=[None,1,D,1], name='x-input') #[M, 1, D, 1]
         # prepare args
         arg = self.get_args(L=3,F=[3,5,7])
+        arg.scope_name = 'BT8D'
         # get NN BT
         bt_mdl = bt_mdl_conv(arg,x)
         # do check
         M = 2
-        X_data = np.array( [np.arange(0,5),np.arange(5,10)] )
+        X_data = np.array( [np.arange(0,9),np.arange(9,17)] )
+        print('X_data ', X_data)
         X_data = X_data.reshape(M,1,D,1)
         with tf.Session() as sess:
             sess.run( tf.initialize_all_variables() )
-            print( sess.run(fetches=bt_mdl, feed_dict={x:X_data}) )
+            print('output: ', sess.run(fetches=bt_mdl, feed_dict={x:X_data}) )
         #self.assertTrue(correct)
 
 if __name__ == '__main__':
