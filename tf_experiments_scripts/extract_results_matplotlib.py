d17b4a01db6e8895d6ff1f5b9acbdc4c1fa2248d
Brando <brando90@mit.edu>
2016-09-22 16:02:48 -0400

    extracted results

--- tf_experiments_scripts/extract_results_matplotlib.py
+++ tf_experiments_scripts/extract_results_matplotlib.py
@@ -13,9 +13,8 @@ import my_tf_pkg as mtf
 
 def display_results4():
     get_errors_from = mtf.get_errors_based_on_train_error
-    # frameworkpython multiple_vs_single_collect_results.py
+    #
     experiment_name = 'om_mnist'
-    #experiment_name = 'om_f_4d_task_conv_2nd'
 
     path_to_experiments = '../../%s/task_August_7_NN1_xavier_momentum'%experiment_name
     nn1_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
@@ -30,14 +29,11 @@ def display_results4():
     nn1_list_units, nn1_list_train_errors, nn1_list_test_errors = mtf.get_list_errors(experiment_results=nn1_multiple_experiment_results,get_errors_from=get_errors_from)
     nn2_list_units, nn2_list_train_errors, nn2_list_test_errors = mtf.get_list_errors(experiment_results=nn2_multiple_experiment_results,get_errors_from=get_errors_from)
     #
-    print('units: ', nn1_list_units)
-    print('nn1_list_test_errors: ', nn1_list_test_errors)
-    print('bt_multiple_experiment_results: ', nn2_list_test_errors)
+    print('get_errors_from = ', get_errors_from.__name__)
+    print('units = ', nn1_list_units)
+    print('nn1_list_test_errors = ', nn1_list_test_errors)
+    print('bt_multiple_experiment_results = ', nn2_list_test_errors)
     #
-    # list_units = np.array(nn1_list_units)
-    # print( list_units)
-    krls.plot_errors(nn1_list_units, nn1_list_test_errors,label='NN1 test', markersize=3, colour='b')
-    krls.plot_errors(nn1_list_units, nn2_list_test_errors,label='NN2 test', markersize=3, colour='c')
     krls.plot_values(nn1_list_units,nn1_list_test_errors,xlabel='number of units',y_label='squared error (l2 loss)',label='NN1 test',markersize=3,colour='b')
     krls.plot_values(nn1_list_units,nn2_list_test_errors,xlabel='number of units',y_label='squared error (l2 loss)',label='NN2 test',markersize=3,colour='c')
     #
@@ -587,16 +583,16 @@ def display_results10():
     plt.show()
 
 def display_results11():
-    get_errors_from = mtf.get_errors_based_on_train_error
-    #
     get_k = lambda a: 7*a + 4*3*2*a**2
     shallow = lambda k: 4*k+k+k
     bt = lambda f: 2*f+f +2*f*(2*f)+ 2*(2*f)
     get_f = lambda a: 3*2*a
+    #
+    get_errors_from = mtf.get_errors_based_on_train_error
 
     task_name = 'task_f_4d_conv_2nd'
     experiment_name = mtf.get_experiment_folder(task_name)
-    #experiment_name = 'om_f_4d_conv_2nd'
+
     (X_train, Y_train, X_cv, Y_cv, X_test, Y_test) = mtf.get_data(task_name)
     var_Y_test = np.var(Y_test)
     print('var_Y_test: ', var_Y_test)
@@ -609,22 +605,29 @@ def display_results11():
     bt_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
     print('LEN(BT)', len(bt_multiple_experiment_results))
 
-    #print nn1_multiple_experiment_results
     nn1_list_units, nn1_list_train_errors, nn1_list_test_errors = mtf.get_list_errors(experiment_results=nn1_multiple_experiment_results,get_errors_from=get_errors_from)
     bt_list_units, bt_list_train_errors, bt_list_test_errors = mtf.get_list_errors(experiment_results=bt_multiple_experiment_results,get_errors_from=get_errors_from)
     #
-    nb_params = [ shallow(nb_units) for nb_units in nn1_list_units ]
+    nb_params_shallow = [ shallow(nb_units) for nb_units in nn1_list_units ]
+    nb_params_bt = [ bt(bt_list_units) for nb_units in bt_list_units ]
+    print('get_errors_from = ', get_errors_from.__name__)
+
     print('shallow units = ', nn1_list_units)
     print('bt_list_units = ', bt_list_units)
+
+    print('nb_params_shallow = ', nb_params_shallow)
+    print('nb_params_bt = ', nb_params_bt)
+
     print('nn1_list_test_errors = ', nn1_list_test_errors)
     print('bt_multiple_experiment_results = ', bt_list_test_errors)
-    print('nb_params = ', nb_params)
+    print('nn1_list_test_errors = ', nn1_list_test_errors)
+    print('bt_multiple_experiment_results = ', bt_list_test_errors)
     # plot train errors
-    krls.plot_values(nb_params,nn1_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN train',markersize=3,colour='b')
-    krls.plot_values(nb_params,bt_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN train',markersize=3,colour='c')
+    krls.plot_values(nb_params_shallow,nn1_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN train',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN train',markersize=3,colour='c')
     # plot test errors
-    krls.plot_values(nb_params,nn1_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN test',markersize=3,colour='b')
-    krls.plot_values(nb_params,bt_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN test',markersize=3,colour='c')
+    krls.plot_values(nb_params_shallow,nn1_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN test',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN test',markersize=3,colour='c')
     #
     plt.legend()
     plt.show()
