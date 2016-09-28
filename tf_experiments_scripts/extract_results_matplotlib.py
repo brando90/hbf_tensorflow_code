2bf5f5f89c477478a939c3be510825d1d0926680
Brando <brando90@mit.edu>
2016-09-28 12:28:55 -0400

    plot recent results

--- tf_experiments_scripts/extract_results_matplotlib.py
+++ tf_experiments_scripts/extract_results_matplotlib.py
@@ -632,6 +632,106 @@ def display_results11():
     plt.legend()
     plt.show()
 
+def display_results12():
+    get_k = lambda a: 7*a + 4*3*2*a**2
+    shallow = lambda k: 4*k+k+k
+    bt = lambda f: 2*f+f +2*f*(2*f)+ 2*(2*f)
+    get_f = lambda a: 3*2*a
+    #
+    get_errors_from = mtf.get_errors_based_on_train_error
+
+    task_name = 'task_f_4d_conv_2nd'
+    experiment_name = mtf.get_experiment_folder(task_name)
+
+    (X_train, Y_train, X_cv, Y_cv, X_test, Y_test) = mtf.get_data(task_name)
+    var_Y_test = np.var(Y_test)
+    print('var_Y_test: ', var_Y_test)
+
+    path_to_experiments = '../../%s/task_September_23_NN_xavier_softplus'%experiment_name
+    nn1_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
+    print('LEN(NN)', len(nn1_multiple_experiment_results))
+
+    path_to_experiments = '../../%s/task_September_23_BTHL_xavier_softplus'%experiment_name
+    bt_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
+    print('LEN(BT)', len(bt_multiple_experiment_results))
+
+    nn1_list_units, nn1_list_train_errors, nn1_list_test_errors = mtf.get_list_errors(experiment_results=nn1_multiple_experiment_results,get_errors_from=get_errors_from)
+    bt_list_units, bt_list_train_errors, bt_list_test_errors = mtf.get_list_errors(experiment_results=bt_multiple_experiment_results,get_errors_from=get_errors_from)
+    #
+    nb_params_shallow = [ shallow(nb_units) for nb_units in nn1_list_units ]
+    nb_params_bt = [ bt(nb_units) for nb_units in bt_list_units ]
+    print('get_errors_from = ', get_errors_from.__name__)
+
+    print('shallow units = ', nn1_list_units)
+    print('bt_list_units = ', bt_list_units)
+
+    print('nb_params_shallow = ', nb_params_shallow)
+    print('nb_params_bt = ', nb_params_bt)
+
+    print('nn1_list_train_errors = ', nn1_list_train_errors)
+    print('bt_list_train_errors = ', bt_list_train_errors)
+    print('nn1_list_test_errors = ', nn1_list_test_errors)
+    print('bt_multiple_experiment_results = ', bt_list_test_errors)
+    # plot train errors
+    krls.plot_values(nb_params_shallow,nn1_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN train',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN train',markersize=3,colour='c')
+    # plot test errors
+    krls.plot_values(nb_params_shallow,nn1_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN test',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN test',markersize=3,colour='c')
+    #
+    plt.legend()
+    plt.show()
+
+def display_results13():
+    get_k = lambda a: 7*a + 4*3*2*a**2
+    shallow = lambda k: 4*k+k+k
+    bt = lambda f: 2*f+f +2*f*(2*f)+ 2*(2*f)
+    get_f = lambda a: 3*2*a
+    #
+    get_errors_from = mtf.get_errors_based_on_train_error
+
+    task_name = 'om_f_4D_simple_ReLu_BT'
+    experiment_name = mtf.get_experiment_folder(task_name)
+
+    (X_train, Y_train, X_cv, Y_cv, X_test, Y_test) = mtf.get_data(task_name)
+    var_Y_test = np.var(Y_test)
+    print('var_Y_test: ', var_Y_test)
+
+    path_to_experiments = '../../%s/task_September_23_NN_xavier_relu'%experiment_name
+    nn1_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
+    print('LEN(NN)', len(nn1_multiple_experiment_results))
+
+    path_to_experiments = '../../%s/task_September_23_BTHL_xavier_relu'%experiment_name
+    bt_multiple_experiment_results = mtf.get_results_for_experiments(path_to_experiments,get_errors_from,verbose=True, split_string='_jBT_[\d]*_|_jHBF[\d]*_|_jrun_HBF[\d]*_|jNN_')
+    print('LEN(BT)', len(bt_multiple_experiment_results))
+
+    nn1_list_units, nn1_list_train_errors, nn1_list_test_errors = mtf.get_list_errors(experiment_results=nn1_multiple_experiment_results,get_errors_from=get_errors_from)
+    bt_list_units, bt_list_train_errors, bt_list_test_errors = mtf.get_list_errors(experiment_results=bt_multiple_experiment_results,get_errors_from=get_errors_from)
+    #
+    nb_params_shallow = [ shallow(nb_units) for nb_units in nn1_list_units ]
+    nb_params_bt = [ bt(nb_units) for nb_units in bt_list_units ]
+    print('get_errors_from = ', get_errors_from.__name__)
+
+    print('shallow units = ', nn1_list_units)
+    print('bt_list_units = ', bt_list_units)
+
+    print('nb_params_shallow = ', nb_params_shallow)
+    print('nb_params_bt = ', nb_params_bt)
+
+    print('nn1_list_train_errors = ', nn1_list_train_errors)
+    print('bt_list_train_errors = ', bt_list_train_errors)
+    print('nn1_list_test_errors = ', nn1_list_test_errors)
+    print('bt_multiple_experiment_results = ', bt_list_test_errors)
+    # plot train errors
+    krls.plot_values(nb_params_shallow,nn1_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN train',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN train',markersize=3,colour='c')
+    # plot test errors
+    krls.plot_values(nb_params_shallow,nn1_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Shallow NN test',markersize=3,colour='b')
+    krls.plot_values(nb_params_bt,bt_list_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Binary Tree NN test',markersize=3,colour='c')
+    #
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
     display_results4()
     #display_results11()
