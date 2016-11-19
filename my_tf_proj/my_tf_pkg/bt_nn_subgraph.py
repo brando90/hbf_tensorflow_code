e50760b0fef162ef7c83a81adfa41686e27960b9
Brando <brando90@mit.edu>
2016-11-19 15:31:51 -0500

    got a major debug issue from convultions to have [?,1,xy,1]

--- my_tf_proj/my_tf_pkg/bt_nn_subgraph.py
+++ my_tf_proj/my_tf_pkg/bt_nn_subgraph.py
@@ -12,6 +12,26 @@ from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
 
 import my_tf_pkg as mtf
 
+def debug_print(l, conv, conv_new, arg):
+    conv_old = conv # old conv before applying conv for the next layer
+    filter_width = arg.list_filter_widths[l] # filter width for current layer
+    nb_filters = arg.nb_filters[l] # nb of filters for current layer
+    stride_width = arg.list_strides[l] # stride_width for current layer
+    print('----')
+    print('-> l ', l)
+    print('conv_old', conv_old)
+    print('conv_new', conv_new)
+    print('arg.list_filter_widths', arg.list_filter_widths)
+    print('arg.nb_filters', arg.nb_filters)
+    print('arg.list_strides', arg.list_strides)
+    print('-')
+    print('filter_width',filter_width)
+    print('nb_filters',nb_filters)
+    print('stride_width',stride_width)
+    print('paddibg', arg.padding)
+    print('\n')
+    #print('number of channels it should have',)
+
 def bt_mdl_conv_subgraph(arg,x):
     '''
     Returns a BT NN.
@@ -23,33 +43,24 @@ def bt_mdl_conv_subgraph(arg,x):
     conv = x
     l=0
     if arg.verbose:
-        print('------------------------------------------------------------------')
-        print('--')
-        print('l ', l)
-        print('arg.F', arg.F)
-        print('nb_filters ', arg.F[l])
-        print('filter_width ', filter_width)
-        print('stride_width ', stride_width)
-        print(conv)
+        debug_print(l,None,conv,arg)
     # make each layer
-    for l in range(1,len(arg.F)):
+    for l in range(1,len(arg.nb_filters)-1):
         filter_width = arg.list_filter_widths[l] # filter width for current layer
         nb_filters = arg.nb_filters[l] # nb of filters for current layer
         stride_width = arg.list_strides[l] # stride_width for current layer
-        if arg.verbose:
-            print('--')
-            print('l ', l)
-            print('arg.F', arg.F)
-            print('nb_filters ', arg.F[l])
-            print('filter_width ', filter_width)
-            print('stride_width ', stride_width)
         #pdb.set_trace()
-        conv = get_activated_conv_layer(arg=arg,x=conv,l=l,filter_width=filter_width,nb_filters=nb_filters,stride_width=stride_width,scope=arg.scope_name+str(l))
+        conv_new = get_activated_conv_layer_subgraph(arg=arg,x=conv,l=l,filter_width=filter_width,nb_filters=nb_filters,stride_width=stride_width,scope=arg.scope_name+str(l))
         if arg.verbose:
-            print(conv)
+            debug_print(l,conv,conv_new,arg)
+        conv = conv_new
+    #pdb.set_trace()
+    l=len(arg.nb_filters)-1
+    if arg.verbose:
+        debug_print(l,conv,None,arg)
     conv = tf.squeeze(conv)
-    fully_connected_filter_width = arg.list_filter_widths[len(arg.F)-1]
-    C = get_final_weight(arg=arg,shape=[fully_connected_filter_width,1],name='C_'+str(len(arg.F)))
+    fully_connected_filter_width = arg.list_filter_widths[l]
+    C = get_final_weight(arg=arg,shape=[fully_connected_filter_width,1],name='C_'+str(len(arg.nb_filters)))
     mdl = tf.matmul(conv,C)
     return mdl
 
@@ -65,6 +76,7 @@ def get_activated_conv_layer_subgraph(arg,x,l,filter_width,nb_filters,stride_wid
     '''
     filter_height, filter_width = [1, filter_width] # filter_height=1 cuz convolutions to vectors
     stride_height, stride_width = [1, stride_width] # stride_height=1 cuz convolutions to vectors
+    print('nb_filters', nb_filters)
     conv = tf.contrib.layers.convolution2d(inputs=x,
         num_outputs=nb_filters,
         kernel_size=[filter_height, filter_width], # [1, filter_width]
@@ -80,23 +92,25 @@ def get_activated_conv_layer_subgraph(arg,x,l,filter_width,nb_filters,stride_wid
         trainable=arg.trainable,
         reuse=False
     )
+    #print(conv)
     #flatten layer
-    conv = flatten_conv_layer(x=conv,l=l,nb_filters=nb_filters,L=len(arg.F)-1)
+    conv = flatten_conv_layer(x=conv)
+    #pdb.set_trace()
     return conv
 
-def flatten_conv_layer(x,l,nb_filters,L):
+def flatten_conv_layer(x):
     '''
-    l = which layer in the BT we want to flatten. This tells use how to flatten because it will tell us how many times we used the convolution in a layer.
-        Equivalently, we apply one convolution locally at the image/vector depending on how many times the function is locally composed.
-        So a binary tree in 4D the layer 1, has 4 = 2^3 - l = 2^2 locations where the function is locally shared.
-    nb_filters = how many units each local convolutions has for this layer.
-    L = number of total (conv) layers the BT network has (not including the approximation layer). Similarly, its just the index of the layer right before the approximation layer.
-        For example, if we have a BT in 8D then we have 3 convolution layers and 1 approximation layer. Thus, we get that L = 3. For a 4D L=2, for 16D L = 4. etc.
-        Note that the final conv layer is just a fully connected layer. i.e. at L = len(arg.F) - 1
+    Since we are doing 1D convs with 4D conv function, we get the convolutions in the shape: [?,1,nb_units,nb_channels]
+    which means that we have "nb_units" and each unit has "nb_channels".
+    To process this we need to flatten it to [?,1,nb_units*nb_channels,1]
+
+    x = convolution vector to flatten
     '''
     #conv = tf.reshape(conv, [-1,1,filter_width*nb_filters,1]) #flat
-    nb_convolution_sections = 2**(L-l)
-    conv = tf.reshape(x, [-1,1,nb_convolution_sections*nb_filters,1]) #flat
+    #nb_convolution_sections = 2**(L-l)
+    #dimensions = [ int(dimension) for dimension in x.get_shape() ]
+    dimensions = x.get_shape()
+    conv = tf.reshape(x, [-1,1,int(dimensions[2])*int(dimensions[3]),1]) #flat
     return conv
 
 def get_final_weight(arg,shape,name='C',dtype=tf.float32,regularizer=None,trainable=True):
@@ -126,7 +140,9 @@ class TestNN_BT(unittest.TestCase):
         L = layers
         nb_filters = list of nb of filters [F^(1),F^(2),...]
         '''
-        arg = ns.Namespace(L=L,verbose=verbose,trainable=True,padding='VALID',scope_name=scope_name)
+        padding = 'VALID'
+        #padding = 'SAME'
+        arg = ns.Namespace(L=L,verbose=verbose,trainable=True,padding=padding,scope_name=scope_name)
         arg.nb_filters = nb_filters
         arg.list_filter_widths = list_filter_widths
         arg.list_strides = list_strides
@@ -138,22 +154,30 @@ class TestNN_BT(unittest.TestCase):
         arg.biases_initializer = tf.constant_initializer(value=0.1, dtype=tf.float32)
         arg.normalizer_fn = None
         #arg.normalizer_fn = tf.contrib.layers.batch_norm
+
+        arg.act = tf.nn.relu
         return arg
 
-    def test_NN_BT8D(self,M=2,D=8,L=3,list_strides=[None,1,1,1]):
-        print('\n -------test'+str(D))
+    def test_NN_BT8D(self,M=3,D=8,L=3):
+        print('\n-------test'+str(D))
         a = 1
+        #F1 = 4*a
         F1 = 4*a
         F2 = 7*a
         F3 = 28*a
         nb_filters=[None,F1,F2,F3]
         u1 = F1
         u2 = F2
-        list_filter_widths=[None,4*u1,4*u2,]
+        u3 = F3
+        list_filter_widths=[None,2,4*u1,4*u2]
+        s1 = 1
+        s2 = 1
+        s3 = 1
+        list_strides=[None,s1,s2,s3]
         #
         x = tf.placeholder(tf.float32, shape=[None,1,D,1], name='x-input') #[M, 1, D, 1]
         # prepare args
-        arg = self.get_args(L=L,nb_filters=nb_filters,list_filter_widths=list_filter_widths,list_strides=list_strides, verbose=False,scope_name='BT_'+str(D)+'D')
+        arg = self.get_args(L=L,nb_filters=nb_filters,list_filter_widths=list_filter_widths,list_strides=list_strides, verbose=True,scope_name='BT_'+str(D)+'D')
         # get NN BT
         bt_mdl = bt_mdl_conv_subgraph(arg,x)
         X_data = self.get_test_data(M,D)
