c4192812cff2809382b5675d548069c643d47c88
Brando <brando90@mit.edu>
2016-11-21 16:48:47 -0500

    corrections

--- tf_experiments_scripts/scripts_for_plotting/extract_results_60K_data_subgraph.py
+++ tf_experiments_scripts/scripts_for_plotting/extract_results_60K_data_subgraph.py
@@ -35,7 +35,9 @@ def task_f_4D_conv_2nd_mdl_relu():
     #transform = 'log'
     plt.subplot(111)
     axes = plt.gca()
-    #
+
+    ########
+    ######## SHALLOW
     path_to_experiments_NN = '../../%s/task_Oct_30_NN_Adam_xavier_relu_N60000'%experiment_name
     #
     expts_best_results = mtf.get_best_results_for_experiments(path_to_experiments_NN,decider,verbose=False)
@@ -65,10 +67,12 @@ def task_f_4D_conv_2nd_mdl_relu():
     #krls.plot_values(nb_params_shallow,sorted_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Test error for Shallow NN',markersize=3,colour='b')
 
     print()
+
+    ########
+    ######## SUBGRAPH
     #path_to_experiments_BT = '../../%s/sb1_8D_Adam_xavier_relu_N6000'%experiment_name
     #path_to_experiments_BT = '../../%s/sb2_8D_Adam_xavier_relu_N6000'%experiment_name
-    #path_to_experiments_BT = '../../%s/sb3_8D_Adam_xavier_relu_N6000'%experiment_name
-    path_to_experiments_BT = '../../%s/task_Oct_30_BT8D_Adam_xavier_relu_N60000'%experiment_name
+    path_to_experiments_BT = '../../%s/sb3_8D_Adam_xavier_relu_N6000'%experiment_name
     expts_best_results = mtf.get_best_results_for_experiments(path_to_experiments_BT,decider,verbose=False,mdl_complexity_criteria='nb_params')
     #pdb.set_trace()
     sorted_units, sorted_train_errors, sorted_validation_errors, sorted_test_errors = mtf.get_errors_for_display(expts_best_results)
@@ -84,6 +88,40 @@ def task_f_4D_conv_2nd_mdl_relu():
     #
     #indices = list(range(0,n))
     indices = list(range(1,n))
+    #
+    nb_params_bt = nb_params_bt[indices]
+    sorted_train_errors = sorted_train_errors[indices]
+    sorted_test_errors = sorted_test_errors[indices]
+    print('SG_params_bt = ', nb_params_bt)
+    print('sorted_train_errors = ', sorted_train_errors)
+    print('sorted_test_errors = ', sorted_test_errors)
+    #sorted_train_errors = sorted_train_errors/worst
+    #sorted_test_errors = sorted_test_errors/worst
+    #sorted_train_errors = np.log(sorted_train_errors)
+    #sorted_test_errors = np.log(sorted_test_errors)
+    krls.plot_values(nb_params_bt,sorted_train_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Train errors for Sub Graph (BT) Neural Net',markersize=3,colour='r')
+    #krls.plot_values(nb_params_bt,sorted_test_errors,xlabel='number of parameters',y_label='squared error (l2 loss)',label='Test errors for Binary Tree (BT) Neural Net',markersize=3,colour='c')
+
+    print()
+
+    ########
+    ######## BT
+    path_to_experiments_BT = '../../%s/task_Oct_30_BT8D_Adam_xavier_relu_N60000'%experiment_name
+    expts_best_results = mtf.get_best_results_for_experiments(path_to_experiments_BT,decider,verbose=False,mdl_complexity_criteria='nb_units')
+    #pdb.set_trace()
+    sorted_units, sorted_train_errors, sorted_validation_errors, sorted_test_errors = mtf.get_errors_for_display(expts_best_results)
+    n = len(sorted_units)
+    nb_params_bt = [ nb_units for nb_units in sorted_units ]
+    if transform == 'log':
+        nb_params_bt = [ np.log(bt(nb_params)) for nb_params in sorted_units ]
+    else:
+        nb_params_bt = [ bt(nb_units) for nb_units in sorted_units ]
+    nb_params_bt = np.array(nb_params_bt)
+    sorted_train_errors = np.array(sorted_train_errors)
+    sorted_test_errors = np.array(sorted_test_errors)
+    #
+    indices = list(range(0,n))
+    #indices = list(range(1,n))
     #
     nb_params_bt = nb_params_bt[indices]
     sorted_train_errors = sorted_train_errors[indices]
