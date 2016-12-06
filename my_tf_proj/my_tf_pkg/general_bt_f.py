c51a9a023b60021f4d0c4efe4d595799a3a268f2
Brando <brando90@mit.edu>
2016-12-05 20:59:36 -0500

    generate general D

--- my_tf_proj/my_tf_pkg/general_bt_f.py
+++ my_tf_proj/my_tf_pkg/general_bt_f.py
@@ -45,6 +45,7 @@ def get_list_of_functions_per_layer_ppt(L):
         h_list = [None, poly1, poly2, cos1, poly3]
     '''
     h_list = [None]
+    params = [None]
     for l in range(1,L+1):
         if l % 3 == 0:
             #trig
@@ -52,13 +53,28 @@ def get_list_of_functions_per_layer_ppt(L):
             freq = np.random.uniform(low=0.9, high=1.7, size=None)
             trig = lambda x: a*np.cos( freq*np.pi*( x[0] + x[1] ) )
             h_list.append(trig)
+            params.append([a,freq,'a*np.cos( freq*np.pi*( x[0] + x[1] ) )'])
         else:
             #poly
-            degree = np.random.randint(2, high=5, size=None)
-            coeff = np.random.uniform(low=0.01, high=3.0, size=2)
-            poly = lambda x: coeff[0]*x[0]+coeff[1]*x[1]
+            degree = np.random.randint(2, high=4, size=None)
+            coeff = np.random.uniform(low=0.01, high=1.3, size=2)
+            poly = lambda x: (coeff[0]*x[0]+coeff[1]*x[1])**degree
             h_list.append(poly)
-    return h_list
+            params.append([degree,coeff,'(coeff[0]*x[0]+coeff[1]*x[1])**degree'])
+    return h_list, params
+
+def get_ppt_function(L):
+    '''
+    gets the function according to ppt structure and the function list that
+    corresponds to each layer and the parameters for those function per layer.
+    '''
+    logD = L
+    D = 2**logD
+    h_list, params = get_list_of_functions_per_layer_ppt(L=logD)
+    f_general = lambda x: f_bt(x,h_list=h_list,l=logD,left=0,right=D)
+    return f_general, h_list, params
+
+##
 
 def get_poly():
     '''
@@ -93,6 +109,37 @@ def _f_btclear_eval_node():
     return
 
 ##
+
+def generate_and_save_data_set_general_D(file_name,f,D, params=None,N_train=60000, N_cv=60000, N_test=60000, low_x=-1, high_x=1):
+    '''
+    Generates a data set according to file
+    note: it also returns the data set to inspect it if desired.
+    '''
+    X_train, Y_train, X_cv, Y_cv, X_test, Y_test = _generate_data_general_D(f,D, params,N_train, N_cv, N_test, low_x, high_x)
+    D = np.array(D)
+    params = np.array([]) if params == None else params
+    np.savez(file_name, file_name,file_name, params=params,D=D,X_train=X_train,Y_train=Y_train, X_cv=X_cv,Y_cv=Y_cv, X_test=X_test,Y_test=Y_test)
+    return X_train, Y_train, X_cv, Y_cv, X_test, Y_test
+
+def _generate_data_general_D(f,D,params=None,N_train=60000, N_cv=60000, N_test=60000, low_x=-1, high_x=1):
+    # train
+    X_train = low_x + (high_x - low_x) * np.random.rand(N_train,D)
+    Y_train = _get_labels_general_D(X_train, f)
+    # CV
+    X_cv = low_x + (high_x - low_x) * np.random.rand(N_cv,D)
+    Y_cv = _get_labels_general_D(X_cv, f)
+    # test
+    X_test = low_x + (high_x - low_x) * np.random.rand(N_test,D)
+    Y_test = _get_labels_general_D(X_test, f)
+    return (X_train, Y_train, X_cv, Y_cv, X_test, Y_test)
+
+def _get_labels_general_D(X,f):
+    N = X.shape[0] # N x D = N x 4
+    Y = np.zeros( (N,1) )
+    for i in range(N):
+        Y[i] = f(X[i])
+    return Y
+#
 
 class TestF_BT(unittest.TestCase):
     #make sure methods start with word test
@@ -130,7 +177,7 @@ class TestF_BT(unittest.TestCase):
 
     def test_BT256D(self, logD=8, N_train=100, low_x=-1, high_x=1):
         D = 2**logD
-        h_list = get_list_of_functions_per_layer_ppt(L=logD)
+        h_list, params = get_list_of_functions_per_layer_ppt(L=logD)
         ##
         f_general = lambda x: f_bt(x,h_list=h_list,l=logD,left=0,right=D)
         ## compare the functions
