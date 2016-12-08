9bc20ae2d51c8bc32a4ea2173c96033bb8ae476c
Brando <brando90@mit.edu>
2016-12-08 18:27:24 -0500

    docker files for my dev

---
+++ docker_files/tensorflow_scripts_test_dockerfile/matrix_multiply.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+# Create a node Constant op that produces a 1x2 matrix.
+matrix1 = tf.constant([[3., 3.]])
+matrix2 = tf.constant([[2.],[2.]])
+
+# Create a node Matmul op
+product = tf.matmul(matrix1, matrix2)
+
+## The Session closes automatically at the end of the with block.
+with tf.Session() as sess:
+  result = sess.run(product)
+  print('print product of matrices in TensorFlow')
+  print(result)
+  #print(type(result))
---
+++ docker_files/tensorflow_scripts_test_dockerfile/test_my_tensorflow_library_imported_correctly.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+import my_tf_pkg as mtf
+
+my_tf_pkg.hello_world()
+my_tf_pkg.test_matrix_multipl()
