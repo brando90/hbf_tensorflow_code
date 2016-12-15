3269b29e85a864b65eea29afaf33c22f1802255f
Brando <brando90@mit.edu>
2016-12-15 00:58:58 -0600

    progress

--- docker_files/tf_cpu/container_setup.sh
+++ docker_files/tf_cpu/container_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-python /hbf_tensorflow_code/my_tf_proj/setup.py develop
+#python3 /hbf_tensorflow_code/my_tf_proj/setup.py develop
+pip3 install /hbf_tensorflow_code/my_tf_proj
 
 #echo $@
+pip3 list
 python3 /hbf_tensorflow_code/docker_files/tf_cpu/batch_main.py $@
 
