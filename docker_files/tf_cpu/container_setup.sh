6215ac815a3ebb30982c8e00ad723daad71a23d5
Brando <brando90@mit.edu>
2016-12-16 01:51:15 -0600

    need to fix the path error

--- docker_files/tf_cpu/container_setup.sh
+++ docker_files/tf_cpu/container_setup.sh
@@ -3,7 +3,8 @@
 #python3 /hbf_tensorflow_code/my_tf_proj/setup.py develop
 pip3 install /hbf_tensorflow_code/my_tf_proj
 
-pip3 list
+#pip3 list
 #echo $@
 #pip list
-python3 /hbf_tensorflow_code/docker_files/tf_cpu/batch_main.py $@
+#python3 /hbf_tensorflow_code/docker_files/tf_cpu/batch_main.py $@
+$@
