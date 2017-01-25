90d740bc9cc18422fb97e5a3b29a6a3be527598b
Brando <brando90@mit.edu>
2017-01-25 14:07:48 -0500

    first run about to happen

--- tf_playground/checkpoint_playground/batch_main_checkpoint_test.py
+++ tf_playground/checkpoint_playground/batch_main_checkpoint_test.py
@@ -6,17 +6,53 @@ mnist = input_data.read_data_sets("tmp_MNIST_data/", one_hot=True)
 import namespaces as ns
 import pdb
 
+#from my_tf_pkg import
+
 import np
+
+def make_and_check_dir(path):
+    '''
+        tries to make dir/file, if it exists already does nothing else creates it.
+    '''
+    try:
+        os.makedirs(path)
+    except OSError:
+        # uncomment to make it raise an error when path is not a directory
+        #if not os.path.isdir(path):
+        #    raise
+        pass
+#
+
+'''
+Note the idea of structure for a ckpt files is as follow:
+
+E.g.
+./all_ckpts/exp_task_name/mdl_NN10/{hp_stid_1/, ..., hp_stid_200/}/mdl_ckpt_file
+
+so ./all_ckpts/exp_task_name/mdl_NN10/ holds the ckpts for job/mdl mdl_NN10. This
+directory holds all the directories for each hyper param. As in mdl_NN10/{hp_stid_1/, ..., hp_stid_200/}.
+Each hyper param has its own true tensorflow ckpt file. So for a single hyper param we have
+mdl_NN10/hp_stid_1/mdl_ckpt_file.
+'''
 
 def ckpts_exist_for_job_mdl(path_to_task_exp):
     '''
     checks if the current job has had any checkpoints saved.
     looks in ./all_ckpts/expt_task/mdl_job and see if the directory exists to see
-    if any ckpts has been created.
+    if any ckpts directory structures has been created. Note it only checks if
+    the directory e.g. ./all_ckpts/exp_task_name/mdl_job exists. If it doesn't
+    its safe to run that specific mdl/job from scratch.
 
-    path_to_task_exp = ./all_ckpts/exp_task_name
+    Note: if the specific experiment ./all_ckpts/exp_task_name has not even been ran
+    obviously neither has ./all_ckpts/exp_task_name/mdl_job been created, so the
+    function returns false (correctly) since we need to start this experiemnt from
+    scratch from the experiment folder (i.e. no job for this mdl has been even ran)
+
+    path_to_task_exp = ./all_ckpts/exp_task_name/mdl_job
     '''
     return os.path.isdir(path_to_task_exp)
+
+#
 
 def get_hp_largest_stid(path_to_folder_with_hps_jobs):
     # For each directory in the tree rooted at directory top (including top itself), it yields a 3-tuple (dirpath, dirnames, filenames).
@@ -32,9 +68,15 @@ def get_largest(hp_dirnames):
     if len(hp_dirnames) == 0:
         return -1
     else:
-        # since the hp_dirnames are in formapt hp_stid_N, try to extract the number
+        # since the hp_dirnames are in format hp_stid_N, try to extract the number and get largest
         largest_stid = np.max([ int(hp_dirname[2]) for hp_dirname in hp_dirnames ])
         return largest_stid
+
+def no_hp_exists(stid):
+    # (stid == -1) means there were no hp that were previously ran
+    return stid == -1
+
+#
 
 def get_latest_and_only_save_path_to_ckpt(largest_stid):
     # get the path where all the ckpts reside
@@ -44,17 +86,27 @@ def get_latest_and_only_save_path_to_ckpt(largest_stid):
     return save_path_to_ckpt
 
 def get_latest_save_path_to_ckpt(largest_stid):
-    # get the path where all the ckpts reside
-    #path_to_folder_with_ckpts = './%s/%s/%s/np_stid_%s'%(arg.root_cktps_folder, arg.experiment_name, arg.job_name, str(largest_stid))
-    # now get the most recent (and only chekpoint) checkpoint
-    #save_path_to_ckpt =
+    # TODO extend to have more than one ckpt
     return get_latest_and_only_save_path_to_ckpt(largest_stid)
+
+#
+
+def has_hp_iteration_ckpt(stid):
+    # For each directory in the tree rooted at directory top (including top itself), it yields a 3-tuple (dirpath, dirnames, filenames).
+    for (dirpath, dirnames, filenames) in os.walk(top=path_to_folder_with_hps_jobs,topdown=True):
+        if dirpath == 'hp_stid_'+stid: # only processes (dirpath == ho_stid_stid) else: (nothing)
+            # filenames = [mdl_ckpt] or [...,mdl_ckpt-N,...]
+            nb_tf_ckpts = len(filenames)
+            return nb_tf_ckpts > 0
+    # it shouldn't really get here, but if it does it probably means we should start this hp from scratch
+    return False
+
 #
 
 def get_args_for_experiment():
     arg = ns.Namespace()
     #
-    arg.root_cktps_folder = './tmp_all_ckpt'
+    arg.root_cktps_folder = 'tmp_all_ckpt'
     arg.experiment_name = 'task_test1'
     arg.job_name = 'mdl_nn10'
     arg.prefix_ckpt = 'mdl_ckpt' # checkpoint prefix
@@ -65,34 +117,42 @@ def main_ckpt(arg):
     '''
     '''
     arg = get_args_for_experiment()
-    # if (there is a ckpt for this experiment continue training) otherwise (start from scratch)
-    if ckpts_exist_for_job_mdl(arg.root_cktps_folder+'/'+arg.experiment_name+'/'+arg.job_name): #/all_ckpts/exp_task_name
+    # if (there is a ckpt structure for this experiment and job continue training) otherwise (start from scratch and run job for experiment)
+    if ckpts_exist_for_job_mdl(arg.root_cktps_folder+'/'+arg.experiment_name+'/'+arg.job_name): # if any of the dirs don't exist then start from scratch
         # continue training, since there is a ckpt for this experiment
         path_to_folder_with_hps_jobs = './%s/%s/%s/'%(arg.root_cktps_folder, arg.experiment_name, arg.job_name)
         largest_stid = get_hp_largest_stid(path_to_folder_with_hps_jobs) # it can be -1
-        if no_hp_exists(stid): # (stid == -1) means need to start hp from scratch
+        if no_hp_exists(largest_stid): # (stid == -1) means there were no hp that were previously ran
             # start training hp from scratch
             arg.start_stid = 1
-            #arg.end_stid = arg.nb_array_jobs
-            save_path_to_ckpt2restore = None
+            arg.end_stid = arg.nb_array_jobs # TODO
+            arg.save_path_to_ckpt2restore = None
+            # mtf.make_and_check_dir(path=path) # technically this should not be needed.
         else:
-            # continue from most recent iteration of that hp
-            arg.start_stid = largest_stid
-            arg.end_stid = arg.nb_array_jobs
-            arg.save_path_to_ckpt2restore = get_latest_save_path_to_ckpt(largest_stid) # /task_exp_name/mdl_nn10/hp_stid_N/ckptK
+            # if (a ckpt iteration exists continue from it) otherwise (start hp iteration from scratch)
+            if has_hp_iteration_ckpt(largest_stid):
+                #start from this specific hp from scratch
+                arg.start_stid = largest_stid
+                arg.end_stid = arg.nb_array_jobs
+                arg.save_path_to_ckpt2restore = get_latest_save_path_to_ckpt(largest_stid) # /task_exp_name/mdl_nn10/hp_stid_N/ckptK
+            else:
+                # else continue from the most recent iteration ckpt
+                arg.start_stid = largest_stid
+                arg.end_stid = arg.nb_array_jobs
+                arg.save_path_to_ckpt2restore = get_latest_save_path_to_ckpt(largest_stid) # /task_exp_name/mdl_nn10/hp_stid_N/ckptK
     else:
-        #start from scratch, since there wasn't a ckpt for this experiment
+        #start from scratch, since there wasn't a ckpt structure for this experiment
         arg.start_stid = 1
-        arg.end_stid = arg.nb_array_jobs
+        arg.end_stid = arg.nb_array_jobs # TODO
         arg.save_path_to_ckpt2restore = None
-    #run_hyperparam_search
+        mtf.make_and_check_dir(path=arg.root_cktps_folder+'/'+arg.experiment_name+'/'+arg.job_name) #first create and make the ckpt directory
     run_hyperparam_search(arg)
 
 ##
 
 def run_hyperparam_search(arg):
-    #do jobs
-    SLURM_ARRAY_TASK_IDS = list(range(int(arg.start_stid),int(arg.nb_array_jobs)))
+    #do hyper_params
+    SLURM_ARRAY_TASK_IDS = list(range(int(arg.start_stid),int(arg.end_stid)))
     for job_array_index in SLURM_ARRAY_TASK_IDS:
         scope_name = 'stid_'+str(job_array_index)
         with tf.variable_scope(scope_name):
@@ -100,6 +160,7 @@ def run_hyperparam_search(arg):
             train(arg)
 
 #
+
 def get_mdl():
     # get model
     W = tf.Variable(tf.truncated_normal([784, 10], mean=0.0, stddev=0.1),name='w')
@@ -108,11 +169,11 @@ def get_mdl():
     return y
 
 def train(arg):
-    #first create and make the ckpt directory
-    mtf.make_and_check_dir(path=path) # TODO
     # placeholder for data
     x = tf.placeholder(tf.float32, [None, 784])
     y_ = tf.placeholder(tf.float32, [None, 10])
+    #
+    y = get_mdl()
     # loss and accuracy
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1)) # list of booleans indicating correct predictions
