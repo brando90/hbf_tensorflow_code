5fdd2cb8f0e01831e0adb9f828be18b67fbeec78
Brando <brando90@mit.edu>
2017-01-27 18:10:18 -0500

    added documentation to code explaining how hp search works

--- tf_playground/checkpoint_playground/batch_main_checkpoint_test.py
+++ tf_playground/checkpoint_playground/batch_main_checkpoint_test.py
@@ -55,6 +55,10 @@ def ckpts_exist_for_job_mdl(path_to_task_exp):
 #
 
 def get_hp_largest_stid(path_to_folder_with_hps_jobs):
+    '''
+    goes to the experiment with the current model folder (as in /all_ckpts/expt_task_name/mdl_nn10/)
+    and gets the most recent stid of the hps.
+    '''
     #pdb.set_trace()
     #For each directory in the tree rooted at directory top (including top itself), it yields a 3-tuple (dirpath, dirnames, filenames).
     for (dirpath, dirnames, filenames) in os.walk(top=path_to_folder_with_hps_jobs,topdown=True):
@@ -64,27 +68,59 @@ def get_hp_largest_stid(path_to_folder_with_hps_jobs):
     # if it gets here it means something bad happened and it starting all the hp's from the first one
     return -1
 
-def get_hp_largest_stid2():
+def get_hp_largest_stid2(path_to_hp):
+    '''
+    goes to the experiment with the current model folder (as in /all_ckpts/expt_task_name/mdl_nn10/)
+    and gets the most recent stid of the hps.
+
+    shows an alternative way to run code using os.listdir(), this way we specify
+    the directory directly and get the list of id's we need. They are basically
+    identical because if the path is correct for either code, the loop for
+    the other code won't even execture because it returns.
+    '''
     # assuming the dir ./all_ckpts/expt_task_name/mdl_NN10/ is not empty and only has directories with hp_stid_N.
-    dirnames = os.listdir() #dirnames = [...,hp_stid_N,...] or []
+    dirnames = os.listdir(path_to_hp) #dirnames = [...,hp_stid_N,...] or []
     largest_stid = get_largest(dirnames)
     return largest_stid
 
 def get_largest(hp_dirnames):
+    '''
+    gets the largest stid of from the hyper params that have been ran. Note that
+    becuase the folder structure has been been decided, it can always extract
+    the stid from the folder name (basically the end is has the stid).
+
+    e.g.
+    - get_largest([hp_stid_1,...,hp_stid_k,...hp_stid_100]) = 100
+    - get_largest([]) = -1
+
+    note: because I am extremely paranoid about counters sometimes starting from 1 or 0
+    I explicitly choose stid=0 not to mean emptiness.
+    '''
     if len(hp_dirnames) == 0:
         return -1
     else:
         # since the hp_dirnames are in format hp_stid_N, try to extract the number and get largest
-        largest_stid = np.max([ int(hp_dirname.split('_')[2]) for hp_dirname in hp_dirnames ])
+        largest_stid = np.max([ int(hp_dirname.split('_')[2]) for hp_dirname in hp_dirnames ]) # extract the stid from string 'hp_stid_N' and then chooses the largest one
         return largest_stid
 
 def no_hp_exists(stid):
+    '''
+    stid map to hp (hyper param) runs that we have made. They are number from
+    1 to N. If no hp has been ran then stid=-1. Thus, this returns true when stid=-1
+    since that means when there are no hp's (i.e. empty [] list of hps) the stid=-1.
+
+    note: because I am extremely paranoid about counters sometimes starting from 1 or 0
+    I explicitly choose stid=0 not to mean emptiness.
+    '''
     # (stid == -1) means there were no hp that were previously ran
     return stid == -1
 
 #
 
 def get_latest_and_only_save_path_to_ckpt(largest_stid):
+    '''
+    gets the path to ckpts
+    '''
     # get the path where all the ckpts reside
     path_to_folder_with_ckpts = './%s/%s/%s/%s'%(arg.root_cktps_folder, arg.experiment_name, arg.job_name, 'hp_stid_'+str(largest_stid))
     # now get the most recent (and only chekpoint) checkpoint
@@ -92,6 +128,12 @@ def get_latest_and_only_save_path_to_ckpt(largest_stid):
     return save_path_to_ckpt
 
 def get_latest_save_path_to_ckpt(largest_stid):
+    '''
+    this was made with the idea that the ckpt files would be appended with
+    maybe a counter or something to start ckpts from the most recent ckpt but
+    save more than 1 ckpt. At this point there is only one chpt saved per hp
+    and the ckpt itself keeps track of the iteration of trainin to continue from.
+    '''
     # TODO extend to have more than one ckpt
     return get_latest_and_only_save_path_to_ckpt(largest_stid)
 
@@ -113,6 +155,19 @@ def does_hp_have_tf_ckpt(path_to_hp):
 
 def main_ckpt(arg):
     '''
+    Runs all the hyper params the we require and restores models if they were
+    interupted at any point by slurm.
+
+    How it works: first if its a model from scratch it tries to check if the experiment/job exists (by looking at existence of folders),
+    if it does not exist then it creates the folders and start training and saving model from scratch. If it does exist then it should
+    check if any hp has been ran in the past. If no hp has been ran then it starts from scratch i.e stid=1.
+    Else, if some hp's have been ran in the past it gets the latest one and continues from there.
+    Note that if the most recent hp has a checkpoint it always loads the checkpoint and continues training
+    from that checkpoint but then proceeds to do other hps if there are more to do.
+
+    note: so if an experiment is re-ran exactly by accident only the last job/hp
+    is re-ran but from the last iteration. Thus, the model might change but the
+    change should be insignificant.
     '''
     # if (there is a ckpt structure for this experiment and job continue training) otherwise (start from scratch and run job for experiment)
     if ckpts_exist_for_job_mdl(arg.root_cktps_folder+'/'+arg.experiment_name+'/'+arg.job_name): # if any of the dirs don't exist then start from scratch
@@ -152,6 +207,12 @@ def main_ckpt(arg):
 ##
 
 def run_hyperparam_search(arg):
+    '''
+    Runs all the hp (hyper param) jobs that it needs to run.
+    Assume that the code calling it has figured out weather it has to restore
+    a model or not. If it does have to restore a model then the stid should be
+    initialized correctly so that it doesn't overwrite old ckpts.
+    '''
     #do hyper_params
     SLURM_ARRAY_TASK_IDS = list(range(int(arg.start_stid),int(arg.end_stid+1)))
     for job_array_index in SLURM_ARRAY_TASK_IDS:
@@ -164,6 +225,9 @@ def run_hyperparam_search(arg):
 #
 
 def get_mdl(x):
+    '''
+    since this is just a test to do the ckpt, it creates a very simple mdl to test.
+    '''
     # get model
     W = tf.Variable(tf.truncated_normal([784, 10], mean=0.0, stddev=0.1),name='w')
     b = tf.Variable(tf.constant(0.1, shape=[10]),name='b')
@@ -171,6 +235,11 @@ def get_mdl(x):
     return y
 
 def execute_hp_array_task(arg):
+    '''
+    executes the current hp (hyper param) slurm array task. Usually means that
+    it has to either continue training a model that wasn't finished training
+    or start one from scratch.
+    '''
     #build graph
     graph = tf.Graph()
     with graph.as_default():
