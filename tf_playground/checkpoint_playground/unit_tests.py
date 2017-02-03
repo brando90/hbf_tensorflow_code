2b0d5e6aabdf47d12741bf408c7f0cd0e5f9f7bb
Brando <brando90@mit.edu>
2017-02-02 23:51:21 -0500

    unit tests

--- tf_playground/checkpoint_playground/unit_tests.py
+++ tf_playground/checkpoint_playground/unit_tests.py
@@ -14,7 +14,7 @@ units tests to write.
   (i.e. check if all hp_stid_N are missing), if non exist then start doing the
   hp runs from the very first one i.e. hp_stid_1 (WORKS)
 
-- check if ./tmp_all_ckpts/exp_task_name/mdl_nn10/hp_stid_N/mdl_ckpt non exist,
+- check if ./tmp_all_ckpts/ezxp_task_name/mdl_nn10/hp_stid_N/mdl_ckpt non exist,
   if no mdl_ckpt (for this hp) then start training this hp_N from the first iteration (WORKS)
 - check from what iteration to start ./tmp_all_ckpts/exp_task_name/mdl_nn10/hp_stid_N/mdl_ckpt
   mdl_ckpt, then continue training forom this iteration (for current hp). (WORKS)
