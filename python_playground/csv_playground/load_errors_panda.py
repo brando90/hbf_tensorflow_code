478ef47a1228e0b11805c82627020fbbcfb12ea2
Brando <brando90@mit.edu>
2017-02-20 14:18:13 -0500

    extact numbers from errors file

--- python_playground/csv_playground/load_errors_panda.py
+++ python_playground/csv_playground/load_errors_panda.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('./tmp_errors/csv_errors_slurm_array_id5')
-print(df)
-print(df.as_matrix())
-print(df.as_matrix().shape)
-print(df.as_matrix()[:,0])
+#print(df)
+print(type( list(df['train_error'])) )
+print(list(df['train_error']) )
+print(type(df['train_error'][0]) )
+print(np.float32( (df['train_error'][0]) ) )
+#print(df.as_matrix().shape)
+#print(df.as_matrix()[:,0])
