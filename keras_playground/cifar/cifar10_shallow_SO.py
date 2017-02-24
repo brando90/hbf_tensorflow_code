3b0078302c8c10c633e8017a9dc185cf14838aa7
Brando <brando90@mit.edu>
2017-02-23 22:21:17 -0500

    keras

---
+++ keras_playground/cifar/cifar10_shallow_SO.py
@@ -0,0 +1,61 @@
+'''Train a simple deep CNN on the CIFAR10 small images dataset.
+GPU run command with Theano backend (with TensorFlow, the GPU is automatically used):
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python cifar10_cnn.py
+It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs.
+(it's still underfitting at that point, though).
+'''
+
+from __future__ import print_function
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+batch_size = 32
+nb_classes = 10
+units_single_layer = 100
+nb_epoch = 200
+data_augmentation = False
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+# The CIFAR10 images are RGB.
+img_channels = 3
+# The data, shuffled and split between train and test sets:
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+#Flatten
+X_train = X_train.reshape((X_train.shape[0],32*32*3))
+X_test = X_test.reshape((X_test.shape[0],32*32*3))
+
+# Convert class vectors to binary class matrices.
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+
+#model.add( Flatten(input_shape=32*32*3) )
+#model.add(Flatten())
+#model.add(Flatten(100, input_shape=(32*32*3,)))
+model.add(Dense(units_single_layer, input_shape=(32*32*3,)))
+model.add(Activation('relu'))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+print('Not using data augmentation.')
+model.fit(X_train, Y_train,
+          batch_size=batch_size,
+          nb_epoch=nb_epoch,
+          validation_data=(X_test, Y_test),
+          shuffle=True)
