daae3cc319df6fe851ab3d1c1f534223f1491182
Brando <brando90@mit.edu>
2017-03-12 21:57:38 -0400

    time added

--- tf_experiments_scripts/dgx1_jobs/run_BT5.sh
+++ tf_experiments_scripts/dgx1_jobs/run_BT5.sh
@@ -5,5 +5,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
+#SBATCH --time=5-18:20
 
 nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/batch_main_BT5.py -sj sj
