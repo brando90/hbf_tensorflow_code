7ecefbcfeb707f55ac7a5c40f87e9db607248452
Brando <brando90@mit.edu>
2017-03-14 16:39:14 -0400

    push jobs

---
+++ tf_experiments_scripts/dgx1_jobs/jobs_64D_binary_product/run_NN31x3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mem=10000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=brando90@mit.edu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=5-18:20
+
+nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/batch_main_NN31x3.py -sj sj
