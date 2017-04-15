dcc6fa00aea878ff5c900062be47149729e74972
Brando Miranda <brando90@mit.edu>
2017-04-15 18:12:02 -0400

    code push

--- tf_experiments_scripts/generate_product_func_binary.py
+++ tf_experiments_scripts/generate_product_func_binary.py
@@ -17,7 +17,7 @@ import time
 
 import pdb
 
-logD = 5
+logD = 6
 D = 2**logD
 f = np.prod
 #
@@ -25,10 +25,10 @@ nbins = 10
 M = 10
 #M = 6*nbins**(2**logD)
 #M = 6*10*(10**6)
-M = 3000000
+M = 3*(10**6)
 #
 type_input_dist = 'full_random_M'
-file_name = '/home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/data/./data/f_%sD_binary_parity_N%s.npz'%(str(D),str(M))
+file_name = '/home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/data/f_%sD_binary_parity_N%s.npz'%(str(D),str(M))
 file_name = './data/f_%sD_binary_parity_N%s.npz'%(str(D),str(M))
 print('D ', D,flush=True)
 print('M ', M,flush=True)
