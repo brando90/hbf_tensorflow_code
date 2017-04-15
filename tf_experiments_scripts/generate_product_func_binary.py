07091489e27dbb50bab225fb794588067b524649
Brando Miranda <brando90@mit.edu>
2017-04-15 16:42:40 -0400

    generate

--- tf_experiments_scripts/generate_product_func_binary.py
+++ tf_experiments_scripts/generate_product_func_binary.py
@@ -17,21 +17,22 @@ import time
 
 import pdb
 
-logD = 6
+logD = 5
 D = 2**logD
 f = np.prod
 #
 nbins = 10
-#M = 6*10
+M = 10
 #M = 6*nbins**(2**logD)
-M = 6*10*(10**6)
+#M = 6*10*(10**6)
+M = 300000
 #
 type_input_dist = 'full_random_M'
-file_name = '/home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/data/./data/f_product_%sD_binary_parity_N%s.npz'%(str(D),str(M))
+file_name = '/home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/data/./data/f_%sD_binary_parity_N%s.npz'%(str(D),str(M))
+file_name = './data/f_%sD_binary_parity_N%s.npz'%(str(D),str(M))
 print('D ', D,flush=True)
 print('M ', M,flush=True)
 print('->f ', file_name,flush=True)
-#file_name = './data/f_product_%sD_binary_parity_N%s.npz'%(str(D),str(M))
 #N_train, N_cv, N_test = N, N, N
 print('file_name ', file_name)
 start_time = time.time()
