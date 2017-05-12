3d5767f569cc25df0c866cabcb45f11ca3a300aa
Brando Miranda <brando90@mit.edu>
2017-05-12 00:39:06 -0400

    tf_gpu_py3_4_2

--- dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/dgx1_jobs/run_BT3.sh
+++ dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/dgx1_jobs/run_BT3.sh
@@ -7,4 +7,4 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=7-00:00
 
-nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/bt3.py -sj sj
+nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3_4_2 python3 /home_simulation_research/hbf_tensorflow_code/dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/bt3.py -sj sj
