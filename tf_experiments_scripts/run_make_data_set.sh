b09d1c2e9eeaec9c6d791081d0cb4f6a5e50e9a0
Brando Miranda <brando90@mit.edu>
2017-05-11 23:45:34 -0400

    wrote sh for sbatch

--- tf_experiments_scripts/run_make_data_set.sh
+++ tf_experiments_scripts/run_make_data_set.sh
@@ -6,4 +6,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=7-00:00
 
-nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/generate_product_func_binary.py
+#nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/generate_product_func_binary.py
+nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/tf_experiments_scripts/generate_product_func.py
