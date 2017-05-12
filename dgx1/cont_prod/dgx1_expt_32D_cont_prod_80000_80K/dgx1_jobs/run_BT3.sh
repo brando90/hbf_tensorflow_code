af5ab5a0f3f6e72d7664bba327a614b7667dbd23
Brando Miranda <brando90@mit.edu>
2017-05-12 00:22:14 -0400

    32D continuous expt

---
+++ dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/dgx1_jobs/run_BT3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mem=50000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=brando90@mit.edu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=7-00:00
+
+nvidia-docker run --rm -v /raid/poggio/home/brando90/home_simulation_research:/home_simulation_research tf_gpu_py3.4 python3 /home_simulation_research/hbf_tensorflow_code/dgx1/cont_prod/dgx1_expt_32D_cont_prod_80000_80K/batch_main_BT3.py -sj sj
